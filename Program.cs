04848bdb5c556e38a6976810856d7bb32556c752
nosike22 <frank.nosike@gmail.com>
2022-02-08 16:37:20 +0100

    Switch Expression Code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Switch_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int course = 5;
+            switch(course)
+            {
+                case 1:
+                    Console.WriteLine("Biology");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Chemistry");
+                    break;
+                case 3:
+                    Console.WriteLine("Maths");
+                    break;
+                case 4:
+                    Console.WriteLine("Physics");
+                    break;
+                case 5:
+                    Console.WriteLine("C#");
+                    break;
+                case 6:
+                    Console.WriteLine("Linux");
+                    break;
+
+            
+
+            }
+        }
+    }
+}
